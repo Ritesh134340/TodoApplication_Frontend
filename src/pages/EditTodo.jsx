0d1e0d5d6e3a47f98153f73c07be2caa54053b40
Ritesh134340 <riteshkumar134340@gmail.com>
2022-11-20 07:05:37 +0530

    update function added

--- src/pages/EditTodo.jsx
+++ src/pages/EditTodo.jsx
@@ -3,6 +3,7 @@ import React, { useEffect,useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch,useSelector } from 'react-redux';
 import {getById,updateTodos} from "../Redux/AppReducer/action"
+import { EditTodoWrapper } from '../styles/edittodo.styled';
 
 
 const EditTodo = () => {
@@ -11,43 +12,104 @@ const EditTodo = () => {
   const User=JSON.parse(localStorage.getItem("profile")) || "";
   const token=User.token;
   const [data,setData]=useState({})
+  const [title,setTitle]=useState("")
+  const [status,setStatus]=useState(false)
+  const [show,setShow]=useState(false)
+  const {isLoading}=useSelector((state)=>{return {isLoading:state.AppReducer.isLoading}})
+  
 
- useEffect(()=>{
-   const id=params.id;
-   const payload={
-    data:{Authorization:`Bearer ${token}`},
-    id:id
-   }
 
-   dispatch(getById(payload)).then((res)=>{
-    setData(res.payload.data)
-   }).catch((err)=>{
-    console.log(err)
-   })
 
- },[])
-
- const handleUpdate=()=>{
+ const handleUpdate=(e)=>{
   const id=params.id
+  e.preventDefault()
+  
+  if(title!==""){
+    console.log(title,status)
+    const payload={
+      data: {Authorization:`Bearer ${token}`},
+      updat:{title:title,
+             status:status
+     },
+  
+      id:id
+     }
+     dispatch(updateTodos(payload)).then((res)=>{
+       console.log(res.payload)
+       const id=params.id;
   const payload={
-   data: {Authorization:`Bearer ${token}`},
-   updat:{},
+   data:{Authorization:`Bearer ${token}`},
    id:id
   }
-  dispatch(updateTodos(payload)).then((res)=>{
-    console.log(res.payload)
+
+  dispatch(getById(payload)).then((res)=>{
+   setData(res.payload.data)
+  }).catch((err)=>{
+   console.log(err)
   })
+       setShow(false)
+     })
+  }
+  
  }
-  console.log(data)
+ useEffect(()=>{
+   
+  const id=params.id;
+  const payload={
+   data:{Authorization:`Bearer ${token}`},
+   id:id
+  }
+
+  dispatch(getById(payload)).then((res)=>{
+   setData(res.payload.data)
+  }).catch((err)=>{
+   console.log(err)
+  })
+
+},[params.id])
+
+  
+   const handleEdit=(e)=>{
+    e.preventDefault()
+    setTitle(data.title)
+    setStatus(data.status)
+    setShow(!show)
+   }
+ 
+
   return (
-    <div>
-       <form>
-        <label>Title</label>
-        <input value={data.title}></input>
-        <label>Status</label>
+   
+      <EditTodoWrapper>
+       
+         
+        <div className='main-edit-div'>
+         {/* {!show && <div className="heading-div"> */}
+         {/* <h3 className="editTitle">Title <br/> <span className="edit-span">{data.title}</span></h3>
+         <h3 className="editTitle">Status</h3>
+         <h4 className="status-heading">{data.status? "Done" : "Not Done"}</h4> */}
+        
+         {/* </div>} */}
+       {  !isLoading && <div className="form-div">
+         <form className="edit-form">
+          <label className="editTitle">Title</label><br/>
+          {!show?<h1>{data.title}</h1> : <input value={title} onChange={(e)=>setTitle(e.target.value)} ></input>}
+          <br/>
+          <label className="editTitle">Status</label><br/>
+          <h5 className="status-heading"> {status ? "Done" : "Not Done"}</h5><button onClickCapture={(e)=>e.preventDefault()} onClick={()=>setStatus(!status)} className="edit-button">Toggle Status</button>
+     
+          
+          <button onClick={handleUpdate}   className="edit-button">Save</button>
+          <button className="edit-button"  onClick={handleEdit}>Edit</button>
+          </form>
+           </div>}
+          
+
+
+     
+        </div>
+       </EditTodoWrapper>
       
-       </form>
-    </div>
+   
   )
 }
 
---
+++ src/styles/edittodo.styled.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const EditTodoWrapper=styled.div`
+      
+     margin-top:80px;
+   
+
+    .main-edit-div{
+        width:500px;
+        margin:auto;
+        box-sizing: border-box;
+    }
+    .edit-form{
+
+    }
+    .heading-div{
+
+    }
+    .form-div{
+   
+       height:300px;
+
+    }
+    .editTitle{
+        font-size:20px;
+        font-family:"Roboto",sans-serif;
+        letter-spacing: 0.6px;
+        font-weight:700;
+
+    }
+    input{
+        height:40px;
+        width:350px;
+        box-sizing: border-box;
+        font-size:17px;
+        padding-left:10px;
+        margin-top:5px;
+        margin-bottom:10px;
+        border-radius:5px;
+        outline:none;
+        border:1px solid gray;
+    }
+    .status-heading{
+        margin-top:15px;
+        margin-bottom:5px;
+        font-size:17px;
+        font-weight:400;
+  
+        font-family:"Roboto",sans-serif
+
+    }
+    .edit-button{
+        margin-top:40px;
+        background-color: none;
+        padding:12px 25px;
+        margin-left:10px;
+        border-radius:8px;
+        font-size:18px;
+        font-weight:bold;
+        border:1px solid transparent;
+        cursor:pointer;
+
+    }
+    .edit-span{
+        font-weight:400;
+        padding-bottom:15px;
+
+    }
+    
+`
